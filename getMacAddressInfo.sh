5a58fb544df20a348090a4b20d4cc9255aef1400
Muzamil Abbasi <muzamil.abbasi@gmail.com>
2022-11-09 19:43:22 -0500

    First Commit

---
+++ getMacAddressInfo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export address=$1
+
+echo "Printing out Company Name for Mac Address"
+curl https://api.macaddress.io/v1\?apiKey\=$API_KEY\&output\=json\&search\=$address  | jq ."vendorDetails | .oui, .companyName" 
+
+
+echo "Writing it down inside a file in $PWD"
+curl https://api.macaddress.io/v1\?apiKey\=$API_KEY\&output\=json\&search\=$address  | jq ."vendorDetails | .oui, .companyName" > companyName
